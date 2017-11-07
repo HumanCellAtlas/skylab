d36f03f1cbd6cf58731e679c9b0a3ed6338ec009
Dave Shiga <dshiga@broadinstitute.org>
2017-11-07 15:17:13 -0500

    Change params to reflect what I've been using for testing

--- scripts/run_deploy_pipelines.sh
+++ scripts/run_deploy_pipelines.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bash deploy_pipelines.sh dev fake_infra_branch fake_10x_version fake_ss2_version
+bash deploy_pipelines.sh dev fake_infra_branch fake_10x_branch fake_ss2_branch
