dd3708f7782732ed8b9b75b783d082019bbf964e
Jishu Xu <jishuxu@broadinstitute.org>
2018-02-21 21:28:40 -0500

    re-write python script and re-format

--- benchmarking/smartseq2/merge_expression_matrix/merge_table_quants.py
+++ benchmarking/smartseq2/merge_expression_matrix/merge_table_quants.py
@@ -5,59 +5,90 @@ import json
 from os.path import basename
 import sys
 import requests
-## set up auth
-client = storage.Client()
-bucket=client.get_bucket('broad-dsde-mint-dev-cromwell-execution')
-## load cromwell credential
-logins=json.load(open('/usr/secrets/broad-dsde-mint-dev-cromwell.json'))
 
-## input json has uuid, run name and value to parse.
-uuid=sys.argv[1]
-run_name=sys.argv[2]
-output_name=sys.argv[4]
-value_name=sys.argv[3]
-##meta_url
-metadata_url="https://cromwell.mint-dev.broadinstitute.org/api/workflows/v1/"+uuid+"/metadata?expandSubWorkflows=false"
-r=requests.get(metadata_url,auth=(logins['cromwell_username'],logins['cromwell_password']))
-data=r.json()
-## load output files
-files=data['outputs'][run_name]
-## parsing
-merged=pd.DataFrame()
-for kk in range(0,len(files)):
-    print(kk)
-    fc1=files[kk]
-    ##samplename=data[kk]['inputs']['sample_name']
-    fc1=fc1.replace('gs://broad-dsde-mint-dev-cromwell-execution/','')
-    blob1=bucket.get_blob(fc1)
-    bname1=basename(fc1)
-    print(bname1)
-    sample_name=bname1.split('.')[0]
-    with open(bname1,'wb') as file_obj:
-        blob1.download_to_file(file_obj)
-    if "unq_genes_counts" in run_name or "mult_genes_counts" in run_name:
-        dat=pd.read_csv(bname1,skiprows=2,sep='\t',usecols=['gene_id','length',sample_name],names=['gene_id','contigs','start','end','strands','length',sample_name])
-        if value_name == 'tpm': ## convert from cnt to tpm
-            sample=list(dat[sample_name].values)
-            lengths=list(dat['length'].values)
-            rpk=[float(s) / float(l) for s,l in zip(sample, lengths)]
-            RPKM=sum(rpk)/1e6
-            TPM=[r/RPKM for r in rpk]
-            dat[sample_name]=TPM
 
-        if kk==0:
-            merged=dat
-        else:
-            cnt=dat
-            merged=pd.merge(left=merged,right=cnt[['gene_id',sample_name]],left_on='gene_id',right_on='gene_id')
-    elif 'rsem' in run_name:
-        if kk==0:
-            merged=pd.read_csv(bname1,delimiter='\t',skiprows=1,sep='\t',names=['gene_id','trans_id', 'length', 'eff_length','est_counts','tpm','fpkm','pme_count','pme_sd','pme_tpm','pme_fpkm'],usecols=['gene_id','length',value_name])
-            merged.columns=['gene_id','length',sample_name]
-        else:
-            cnt=pd.read_csv(bname1,delimiter='\t',skiprows=1,sep='\t',names=['gene_id','trans_id', 'length', 'eff_length','est_counts','tpm','fpkm','pme_count','pme_sd','pme_tpm','pme_fpkm'],usecols=['gene_id',value_name])
-            cnt.columns=['gene_id',sample_name]
-            merged=pd.merge(left=merged,right=cnt,left_on='gene_id',right_on='gene_id')
-merged=merged.round(3)
-merged.to_csv(output_name,index=False)
+def merge_expression_matrix(uuid, run_name,output_name,value_name):
+    """
+    pipeline produce gene quantification by using either FeatureCounts or RSEM at single sample level 
+    This function is called to merge or aggregate all the single cell/sample level results into
+    a single matrix file
+    param uuid: the uuid of pipeline run, top level uuid 
+    param run_name: run_name is the output name in pipeline metadata, for exampel, run_pipelines.RunHisat2Pipeline.unq_genes_counts
+    param output_name: output file name, ex STAR_tpm.csv
+    para value_name: aggregate value type, can be est_count or tpm. 
+    """
+    # set up auth
+    client = storage.Client()
+    bucket = client.get_bucket('broad-dsde-mint-dev-cromwell-execution')
+    # load cromwell credential
+    logins = json.load(open('/usr/secrets/broad-dsde-mint-dev-cromwell.json'))
+    # meta_url
+    metadata_url = "https://cromwell.mint-dev.broadinstitute.org/api/workflows/v1/"+uuid+"/metadata?expandSubWorkflows=false"
+    r = requests.get(metadata_url, auth=(logins['cromwell_username'], logins['cromwell_password']))
+    data = r.json()
+    # load output files
+    files = data['outputs'][run_name]
+    # initial dataframe
+    merged = pd.DataFrame()
+    # loop through input files
+    for kk in range(0, len(files)):
+        print(kk)
+        fc1 = files[kk]
+        fc1 = fc1.replace('gs://broad-dsde-mint-dev-cromwell-execution/', '')
+        blob1 = bucket.get_blob(fc1)
+        bname1 = basename(fc1)
+        print(bname1)
+        # sample_name is the prefix of input file name, such as SRR123456
+        sample_name = bname1.split('.')[0]
+        with open(bname1, 'wb') as file_obj:
+            blob1.download_to_file(file_obj)
+        # if it is featurecount output, there should be unq_genes_counts and mult_genes_counts in file name
+        if "unq_genes_counts" in run_name or "mult_genes_counts" in run_name:
+            # load file and only select gene_id, lenght and sample column
+            dat = pd.read_csv(bname1, skiprows=2, sep='\t', usecols=['gene_id', 'length', sample_name], names=['gene_id', 'contigs', 'start', 'end', 'strands', 'length', sample_name])
+            # featurecount only output counts so need to convert counts to tpm
+            if value_name == 'tpm':  
+                sample = list(dat[sample_name].values)
+                lengths = list(dat['length'].values)
+                rpk = [float(s) / float(l) for s, l in zip(sample, lengths)]
+                RPKM = sum(rpk)/1e6
+                TPM = [r/RPKM for r in rpk]
+                # replace count values by tpm values
+                dat[sample_name] = TPM
+            # while looping input files, merging parsed results into a single file as output
+            # initial merged file by the first input file
+            if kk == 0:
+                merged = dat
+            else:
+                # only parse out gene_id and sample column from input files if it is not the first input file. 
+                cnt = dat
+                # merge by gene_id
+                merged = pd.merge(left=merged, right=cnt[['gene_id', sample_name]], left_on='gene_id', right_on='gene_id')
+        # if input is from rsem. RSEM produce est_count for expected_count and tpm. so no need to convert count to tpm then
+        elif 'rsem' in run_name:
+            # same as previous. only select gene_id, length and sample column. 
+            if kk == 0:
+                merged = pd.read_csv(bname1, delimiter='\t', skiprows=1, sep='\t', names=['gene_id', 'trans_id', 'length', 'eff_length', 'est_counts', 'tpm', 'fpkm', 'pme_count', 'pme_sd', 'pme_tpm', 'pme_fpkm'], usecols=['gene_id', 'length', value_name])
+                merged.columns = ['gene_id', 'length', sample_name]
+            else:
+                cnt = pd.read_csv(bname1, delimiter='\t', skiprows=1, sep='\t', names=['gene_id', 'trans_id', 'length', 'eff_length', 'est_counts', 'tpm', 'fpkm', 'pme_count', 'pme_sd', 'pme_tpm', 'pme_fpkm'], usecols=['gene_id', value_name])
+                cnt.columns = ['gene_id', sample_name]
+                merged = pd.merge(left=merged, right=cnt, left_on='gene_id', right_on='gene_id')
+    # round matrix by 3 digits
+    merged = merged.round(3)
+    # output to csv
+    merged.to_csv(output_name, index=False)
 
+def main():
+     # input json has uuid, run name and value to parse.
+    uuid = sys.argv[1]
+    # pipeline task name, such as STARpipeline.rsem
+    run_name = sys.argv[2]
+    # output file name
+    output_name = sys.argv[4]
+    # file or quantification name, such as est_cnt for counts data, tpm, fpkm
+    value_name = sys.argv[3]
+    merge_expression_matrix(uuid, run_name,output_name,value_name)
+
+if __name__ == "__main__":
+    main()
--- benchmarking/smartseq2/merge_picard_metrics/merge_picard_mets.py
+++ benchmarking/smartseq2/merge_picard_metrics/merge_picard_mets.py
@@ -7,46 +7,67 @@ from os.path import basename
 import sys
 import requests
 
-## set up auth
-client = storage.Client()
-bucket=client.get_bucket('broad-dsde-mint-dev-cromwell-execution')
-## load cromwell credential
-logins=json.load(open('/usr/secrets/broad-dsde-mint-dev-cromwell.json'))
-
-## input json has uuid, run name and value to parse.
-uuid=sys.argv[1]
-met_name=sys.argv[2]
-output_name=sys.argv[3]
-##meta_url
-metadata_url="https://cromwell.mint-dev.broadinstitute.org/api/workflows/v1/"+uuid+"/metadata?expandSubWorkflows=false"
-print(metadata_url)
-r=requests.get(metadata_url,auth=(logins['cromwell_username'],logins['cromwell_password']))
-data=r.json()
-## load output files
-files=data['outputs'][met_name]
-## parsing
-mets={}
-for kk in range(0,len(files)):
-    fc1=files[kk]
-    fc1=fc1.replace('gs://broad-dsde-mint-dev-cromwell-execution/','')
-    blob1=bucket.get_blob(fc1)
-    bname1=basename(fc1)
-    sample_name=bname1.split('.')[0]
-    with open(bname1,'wb') as file_obj:
-        blob1.download_to_file(file_obj)
-    parsed = picard.parse(bname1)
-    class_name = parsed['metrics']['class']
-    if class_name == "picard.analysis.AlignmentSummaryMetrics":
-        ## only parse out pair reads 
-        met = parsed['metrics']['contents'][2]
-    elif class_name == "picard.analysis.InsertSizeMetrics":
-        if len(parsed['metrics']['contents']) <21:
-            met = parsed['metrics']['contents'][0]
+def merge_picard_metrics(uuid,met_name,output_name):
+    """
+    piepline output picard QC metrics at sinle cell/sample level.
+    This functin is called to merge/aggregate QC metrics by metrics type and then merge multiple QC measurement 
+    into single matrix file. In this file, column is sample/cell and row is QC metrics
+    param uuid: run uuid
+    param met_name: metrics name with workflow name and subworkflow name as prefix. such as ['run_pipelines.RunStarPipeline.alignment_summary_metrics']
+    param output_name: output name, csv file
+    """
+    # set up auth
+    client = storage.Client()
+    bucket = client.get_bucket('broad-dsde-mint-dev-cromwell-execution')
+    # load cromwell credential
+    logins = json.load(open('/usr/secrets/broad-dsde-mint-dev-cromwell.json'))
+    # meta_url
+    metadata_url = "https://cromwell.mint-dev.broadinstitute.org/api/workflows/v1/"+uuid+"/metadata?expandSubWorkflows=false"
+    print(metadata_url)
+    r = requests.get(metadata_url,auth=(logins['cromwell_username'], logins['cromwell_password']))
+    data = r.json()
+    # load output files
+    files = data['outputs'][met_name]
+    # initial output
+    mets = {}
+    for kk in range(0,len(files)):
+        fc1 = files[kk]
+        fc1 = fc1.replace('gs://broad-dsde-mint-dev-cromwell-execution/','')
+        blob1 = bucket.get_blob(fc1)
+        bname1 = basename(fc1)
+        # sample name is prefix of file name
+        sample_name = bname1.split('.')[0]
+        with open(bname1,'wb') as file_obj:
+            blob1.download_to_file(file_obj)
+        # use picard package parse out picard output, a json file is returned
+        parsed = picard.parse(bname1)
+        class_name = parsed['metrics']['class']
+        # Aignment metrics return multiple lines, but only output PAIRED-READS/third line
+        if class_name == "picard.analysis.AlignmentSummaryMetrics":
+            ## only parse out pair reads 
+            met = parsed['metrics']['contents'][2]
+        # sometimes(very rare), insertion metrics also return multiple lines results to include TANDEM repeats. but we only output the first line. 
+        elif class_name == "picard.analysis.InsertSizeMetrics":
+            # if the elemnet counts is less than 21, it means insertion metrics returns multiple line results.
+            if len(parsed['metrics']['contents']) <21:
+                met = parsed['metrics']['contents'][0]
+            else:
+                met = parsed['metrics']['contents']
         else:
+            # other metrics(so far) only return one line results.
             met = parsed['metrics']['contents']
-    else:
-        met = parsed['metrics']['contents']
-    mets[sample_name] = met
-tab = pd.DataFrame.from_dict(mets)
-tab.to_csv(output_name)
+        mets[sample_name] = met
+    tab = pd.DataFrame.from_dict(mets)
+    tab.to_csv(output_name)
 
+def main():
+    ## input json has uuid, run name and value to parse.
+    uuid = sys.argv[1]
+    # metrics name, such as STARpipeline.alignemntmetrics
+    met_name = sys.argv[2]
+    # output file name, such as STAR_Aln.csv
+    output_name = sys.argv[3]
+    merge_picard_metrics(uuid,met_name,output_name)
+
+if __name__ == "__main__":
+    main()
