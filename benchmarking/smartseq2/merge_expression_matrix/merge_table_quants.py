8e138f3026b957778c73f5c7c26d024e8611844d
Jishu Xu <jishuxu@broadinstitute.org>
2018-03-01 14:13:20 -0500

    re-style python codes

--- benchmarking/smartseq2/merge_expression_matrix/merge_table_quants.py
+++ benchmarking/smartseq2/merge_expression_matrix/merge_table_quants.py
@@ -7,22 +7,29 @@ from os.path import basename
 import sys
 import requests
 
+
 def retrieve_workflow_outputs(uuid, run_name):
     # load cromwell credential
     logins = json.load(open('/usr/secrets/broad-dsde-mint-dev-cromwell.json'))
     # meta_url
-    metadata_url = "https://cromwell.mint-dev.broadinstitute.org/api/workflows/v1/"+uuid+"/metadata?expandSubWorkflows=false"
-    r = requests.get(metadata_url, auth=(logins['cromwell_username'], logins['cromwell_password']))
+    metadata_url = "https://cromwell.mint-dev.broadinstitute.org/api/workflows/v1/" + uuid + "/metadata?expandSubWorkflows=false"
+    r = requests.get(
+        metadata_url,
+        auth=(logins['cromwell_username'], logins['cromwell_password']))
     data = r.json()
     # load output files
     files = data['outputs'][run_name]
-    return(files)
+    return (files)
+
+
 def counts_to_tpm(counts, lengths):
     rpk = [float(s) / float(l) for s, l in zip(counts, lengths)]
-    RPKM = sum(rpk)/1e6
-    TPM = [r/RPKM for r in rpk]
-    return(TPM)
-def merge_featurecount_outputs(files, value_name) :
+    RPKM = sum(rpk) / 1e6
+    TPM = [r / RPKM for r in rpk]
+    return (TPM)
+
+
+def merge_featurecount_outputs(files, value_name):
     """
     pipeline produce gene quantification by using either FeatureCounts at single sample level 
     This function is called to merge or aggregate all the single cell/sample level results into
@@ -36,7 +43,7 @@ def merge_featurecount_outputs(files, value_name) :
     # initial dataframe
     merged = pd.DataFrame()
     # loop through input files
-    for kk in range(0,len(files)): 
+    for kk in range(0, len(files)):
         fc1 = files[kk]
         fc1 = fc1.replace('gs://broad-dsde-mint-dev-cromwell-execution/', '')
         blob1 = bucket.get_blob(fc1)
@@ -46,7 +53,15 @@ def merge_featurecount_outputs(files, value_name) :
         with open(bname1, 'wb') as file_obj:
             blob1.download_to_file(file_obj)
         # load file and only select gene_id, lenght and sample column
-        dat = pd.read_csv(bname1, skiprows=2, sep='\t', usecols=['gene_id', 'length', sample_name], names=['gene_id', 'contigs', 'start', 'end', 'strands', 'length', sample_name])
+        dat = pd.read_csv(
+            bname1,
+            skiprows=2,
+            sep='\t',
+            usecols=['gene_id', 'length', sample_name],
+            names=[
+                'gene_id', 'contigs', 'start', 'end', 'strands', 'length',
+                sample_name
+            ])
         # featurecount only output counts so need to convert counts to tpm
         if value_name == 'tpm':
             sample = list(dat[sample_name].values)
@@ -61,9 +76,14 @@ def merge_featurecount_outputs(files, value_name) :
             # only parse out gene_id and sample column from input files if it is not the first input file.
             cnt = dat
             # merge by gene_id
-            merged = pd.merge(left=merged, right=cnt[['gene_id', sample_name]], left_on='gene_id', right_on='gene_id')
+            merged = pd.merge(
+                left=merged,
+                right=cnt[['gene_id', sample_name]],
+                left_on='gene_id',
+                right_on='gene_id')
     merged = merged.round(3)
     return (merged)
+
 
 def merge_rsem_outputs(files, value_name):
     """
@@ -90,15 +110,37 @@ def merge_rsem_outputs(files, value_name):
             blob1.download_to_file(file_obj)
         # if it is first iteration, need to parse out header
         if kk == 0:
-            merged = pd.read_csv(bname1, delimiter='\t', skiprows=1, sep='\t', names=['gene_id', 'trans_id', 'length', 'eff_length', 'est_counts', 'tpm', 'fpkm', 'pme_count', 'pme_sd', 'pme_tpm', 'pme_fpkm'], usecols=['gene_id', 'length', value_name])
+            merged = pd.read_csv(
+                bname1,
+                delimiter='\t',
+                skiprows=1,
+                sep='\t',
+                names=[
+                    'gene_id', 'trans_id', 'length', 'eff_length',
+                    'est_counts', 'tpm', 'fpkm', 'pme_count', 'pme_sd',
+                    'pme_tpm', 'pme_fpkm'
+                ],
+                usecols=['gene_id', 'length', value_name])
             merged.columns = ['gene_id', 'length', sample_name]
         else:
-            cnt = pd.read_csv(bname1, delimiter='\t', skiprows=1, sep='\t', names=['gene_id', 'trans_id', 'length', 'eff_length', 'est_counts', 'tpm', 'fpkm', 'pme_count', 'pme_sd', 'pme_tpm', 'pme_fpkm'], usecols=['gene_id', value_name])
+            cnt = pd.read_csv(
+                bname1,
+                delimiter='\t',
+                skiprows=1,
+                sep='\t',
+                names=[
+                    'gene_id', 'trans_id', 'length', 'eff_length',
+                    'est_counts', 'tpm', 'fpkm', 'pme_count', 'pme_sd',
+                    'pme_tpm', 'pme_fpkm'
+                ],
+                usecols=['gene_id', value_name])
             cnt.columns = ['gene_id', sample_name]
-            merged = pd.merge(left=merged, right=cnt, left_on='gene_id', right_on='gene_id')
+            merged = pd.merge(
+                left=merged, right=cnt, left_on='gene_id', right_on='gene_id')
     # round matrix by 3 digits
     merged = merged.round(3)
-    return(merged)
+    return (merged)
+
 
 def run_merge_expression(uuid, run_name, output_name, value_name):
     """
@@ -116,15 +158,39 @@ def run_merge_expression(uuid, run_name, output_name, value_name):
         merged_matrix = merge_rsem_outputs(vfiles, value_name)
     merged_matrix.to_csv(output_name, index=False)
 
+
 def main():
-    
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--uuid", dest = "uuid", required = True, help = "The uuid of workflow, top level")
-    parser.add_argument("-rn", "--run_name", dest = "run_name", required = True, help = "The output file name from task or workflow,ex RunSTARPipeline.rsem_gene_results")
-    parser.add_argument("-o", "--output_name", dest = "output_name", required = True, help = "The output folder folder name.")
-    parser.add_argument("-t", "--value_name", dest = "value_name", required = True, help="value or column to parse, ex, tpm, est_counts")
+    parser.add_argument(
+        "-u",
+        "--uuid",
+        dest="uuid",
+        required=True,
+        help="The uuid of workflow, top level")
+    parser.add_argument(
+        "-rn",
+        "--run_name",
+        dest="run_name",
+        required=True,
+        help=
+        "The output file name from task or workflow,ex RunSTARPipeline.rsem_gene_results"
+    )
+    parser.add_argument(
+        "-o",
+        "--output_name",
+        dest="output_name",
+        required=True,
+        help="The output folder folder name.")
+    parser.add_argument(
+        "-t",
+        "--value_name",
+        dest="value_name",
+        required=True,
+        help="value or column to parse, ex, tpm, est_counts")
     args = parser.parse_args()
-    run_merge_expression(args.uuid, args.run_name,args.output_name,args.value_name)
+    run_merge_expression(args.uuid, args.run_name, args.output_name,
+                         args.value_name)
+
 
 if __name__ == "__main__":
     main()
--- benchmarking/smartseq2/merge_picard_metrics/merge_picard_mets.py
+++ benchmarking/smartseq2/merge_picard_metrics/merge_picard_mets.py
@@ -8,18 +8,22 @@ import sys
 import requests
 import argparse
 
+
 def retrieve_workflow_outputs(uuid, run_name):
     # load cromwell credential
     logins = json.load(open('/usr/secrets/broad-dsde-mint-dev-cromwell.json'))
     # meta_url
-    metadata_url = "https://cromwell.mint-dev.broadinstitute.org/api/workflows/v1/"+uuid+"/metadata?expandSubWorkflows=false"
-    r = requests.get(metadata_url, auth=(logins['cromwell_username'], logins['cromwell_password']))
+    metadata_url = "https://cromwell.mint-dev.broadinstitute.org/api/workflows/v1/" + uuid + "/metadata?expandSubWorkflows=false"
+    r = requests.get(
+        metadata_url,
+        auth=(logins['cromwell_username'], logins['cromwell_password']))
     data = r.json()
     # load output files
     files = data['outputs'][run_name]
-    return(files)
+    return (files)
 
-def merge_picard_metrics(files,metric_name):
+
+def merge_picard_metrics(files, metric_name):
     """
     piepline output picard QC metrics at sinle cell/sample level.
     This functin is called to merge/aggregate QC metrics by metrics type and then merge multiple QC measurement 
@@ -34,26 +38,26 @@ def merge_picard_metrics(files,metric_name):
     logins = json.load(open('/usr/secrets/broad-dsde-mint-dev-cromwell.json'))
     # initial output
     mets = {}
-    for kk in range(0,len(files)):
+    for kk in range(0, len(files)):
         fc1 = files[kk]
-        fc1 = fc1.replace('gs://broad-dsde-mint-dev-cromwell-execution/','')
+        fc1 = fc1.replace('gs://broad-dsde-mint-dev-cromwell-execution/', '')
         blob1 = bucket.get_blob(fc1)
         bname1 = basename(fc1)
         # sample name is prefix of file name
         sample_name = bname1.split('.')[0]
-        with open(bname1,'wb') as file_obj:
+        with open(bname1, 'wb') as file_obj:
             blob1.download_to_file(file_obj)
         # use picard package parse out picard output, a json file is returned
         parsed = picard.parse(bname1)
         class_name = parsed['metrics']['class']
         # Aignment metrics return multiple lines, but only output PAIRED-READS/third line
         if class_name == "picard.analysis.AlignmentSummaryMetrics":
-            ## only parse out pair reads 
+            ## only parse out pair reads
             met = parsed['metrics']['contents'][2]
-        # sometimes(very rare), insertion metrics also return multiple lines results to include TANDEM repeats. but we only output the first line. 
+        # sometimes(very rare), insertion metrics also return multiple lines results to include TANDEM repeats. but we only output the first line.
         elif class_name == "picard.analysis.InsertSizeMetrics":
             # if the elemnet counts is less than 21, it means insertion metrics returns multiple line results.
-            if len(parsed['metrics']['contents']) <21:
+            if len(parsed['metrics']['contents']) < 21:
                 met = parsed['metrics']['contents'][0]
             else:
                 met = parsed['metrics']['contents']
@@ -62,21 +66,41 @@ def merge_picard_metrics(files,metric_name):
             met = parsed['metrics']['contents']
         mets[sample_name] = met
     tab = pd.DataFrame.from_dict(mets)
-    return(tab)
+    return (tab)
 
-def run_merge_metrics(uuid, metric_name, output_name): 
+
+def run_merge_metrics(uuid, metric_name, output_name):
+    """
+    call functions to nerge metrics and output in one file
+    """
     metfiles = retrieve_workflow_outputs(uuid, metric_name)
-    metrics_matrix = merge_picard_metrics(metfiles,metric_name)
+    metrics_matrix = merge_picard_metrics(metfiles, metric_name)
     metrics_matrix.to_csv(output_name)
 
-def main():
 
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--uuid", dest = "uuid", required = True, help = "The uuid of workflow, top level")
-    parser.add_argument("-m", "--metrics_name", dest = "met_name", required = True, help = "The Picard metrics class name")
-    parser.add_argument("-o", "--output_name", dest = "output_name", required = True, help = "The output file name")
+    parser.add_argument(
+        "-u",
+        "--uuid",
+        dest="uuid",
+        required=True,
+        help="The uuid of workflow, top level")
+    parser.add_argument(
+        "-m",
+        "--metrics_name",
+        dest="met_name",
+        required=True,
+        help="The Picard metrics class name")
+    parser.add_argument(
+        "-o",
+        "--output_name",
+        dest="output_name",
+        required=True,
+        help="The output file name")
     args = parser.parse_args()
     run_merge_metrics(args.uuid, args.met_name, args.output_name)
+
 
 if __name__ == "__main__":
     main()
