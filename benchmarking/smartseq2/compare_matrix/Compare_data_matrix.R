d2471683f35cdd7cffa5eeabdaddf4ac8158d4f0
Jishu Xu <jishuxu@broadinstitute.org>
2018-03-28 22:54:27 -0400

    update analysis

--- benchmarking/smartseq2/compare_matrix/Compare_data_matrix.R
+++ benchmarking/smartseq2/compare_matrix/Compare_data_matrix.R
@@ -41,7 +41,6 @@ opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 matrixfile1 <- opt$matrix1
 matrixfile2 <- opt$matrix2
-gtf_file <-  opt$gtf_file
 metadata_file <- opt$metadata_file
 output_name <- opt$output_prefix
 # color palette
@@ -49,30 +48,30 @@ palette(c("#00AFBB", "#E7B800"))
 # load data matrix
 mat1 <- read.csv(matrixfile1)
 mat2 <- read.csv(matrixfile2)
-rownames(mat1)<-mat1[,1]
-rownames(mat2)<-mat2[,1]
-mat1.d<-mat1[,-c(1:2)]
-mat2.d<-mat2[,-c(1:2)]
+rownames(mat1) <- mat1[, 1]
+rownames(mat2) <- mat2[, 1]
+mat1.d <- mat1[, -c(1:2)]
+mat2.d <- mat2[, -c(1:2)]
 #match row
 mlist <- match(rownames(mat1.d), rownames(mat2.d))
 # match sample column
 nlist <- match(colnames(mat1.d), colnames(mat2.d))
 mat2.d <- mat2.d[mlist, nlist]
 # meta
-meta <- read.csv(metadata_file,header=T)
+meta <- read.csv(metadata_file, header = T)
 mlist <- match(colnames(mat1.d), meta$sra)
-grps <- meta[mlist, 'cell_type']
+meta <- meta[mlist, ]
 
 #log2 transformation
-mat1.log2<-takelog2(mat1.d)
-mat2.log2<-takelog2(mat2.d)
+mat1.log2 <- takelog2(mat1.d)
+mat2.log2 <- takelog2(mat2.d)
 
 # correlation
 # return two matrix of corraltion matrix
 # take diag of correlation matrix
 # plot into histogram
-mcor<-cor(mat1.log2,mat2.log2)
-cxy<-diag(mcor)
+mcor <- cor(mat1.log2, mat2.log2)
+cxy <- diag(mcor)
 phist <- gghistogram(
   cxy,
   fill = "lightgray",
@@ -80,16 +79,28 @@ phist <- gghistogram(
   rug = TRUE,
   ggtheme = theme_minimal(),
   xlab = "Correlation: Expression of Base vs Updated pipeline"
-)
+) + border()
 phist <- addTheme(phist)
 # SNN cluster
-mat1.snn <- RunSNNCluster(mat1)
-mat2.snn <- RunSNNCluster(mat2)
-# Calculate adjusted rand index between two cluster results
-# to figure out how similar the results are
-# ideal, if we habe pre-labeled data, we can calculate RAND index
-# between clustering result and labels
-rand <- adjustedRandIndex(mat1.snn$membership, mat2.snn$membership)
+mat1.snn <- RunSNNCluster(mat1.log2)
+mat2.snn <- RunSNNCluster(mat2.log2)
+# Calculate adjusted rand index between clustering results to metadata
+labels <- paste(meta$lineage, meta$cell, sep = '-')
+r1 <- adjustedRandIndex(mat1.snn$membership,
+                        labels)
+r2 <- adjustedRandIndex(mat2.snn$membership,
+                        labels)
+t1 <- table(mat1.snn$membership, labels)
+t2 <- table(mat2.snn$membership, labels)
+
+dtable.1 <-
+  ggtexttable(t1,
+              theme = ttheme('mBlue', base_size = 18),
+              rows = rownames(t1))
+dtable.2 <-
+  ggtexttable(t2,
+              theme = ttheme('mBlue', base_size = 18),
+              rows = rownames(t2))
 
 # visualize SNN clustering
 snn.p1 <- ggscatter(
@@ -116,18 +127,35 @@ snn.p2 <- ggscatter(
 ) + border()
 snn.p1 <- addTheme(snn.p1)
 snn.p2 <- addTheme(snn.p2)
-pcl <- ggarrange(snn.p1,
-                snn.p2,
-                ncol = 2,
-                nrow = 1)
-
+p1 <- ggarrange(
+  snn.p1,
+  dtable.1,
+  ncol = 1,
+  nrow = 2,
+  heights = c(2, 1),
+  labels = c(
+    paste('A: Clustering of base pipeline:Rand Index:', r1),
+    "B: clustering index vs sample labels"
+  )
+)
+p2 <- ggarrange(
+  snn.p2,
+  dtable.2,
+  ncol = 1,
+  nrow = 2,
+  heights = c(2, 1),
+  labels = c(
+    paste('A: Clustering of updated pipeline:Rand Index:', r2),
+    "B: clustering index vs sample labels"
+  )
+)
 pdf(paste(output_name, '_cluster_data_matrix_comparison.pdf', sep = ''),
-    30,
-    60)
+    20,
+    15)
 print(phist)
-print(pcl)
+print(p1)
+print(p2)
 dev.off()
-
 
 grps <-
   merge(mat1.snn,
--- benchmarking/smartseq2/compare_matrix/run_matrix_comparison.wdl
+++ benchmarking/smartseq2/compare_matrix/run_matrix_comparison.wdl
@@ -1,7 +1,7 @@
 task CompareDataMatrix {
   File base_datafile
   File updated_datafile
-  File gtf_file
+  File metadata_file
   String output_name
   String src_dir
   
@@ -13,7 +13,7 @@ task CompareDataMatrix {
     Rscript Compare_data_matrix.R \
       --matrix1 ${base_datafile} \
       --matrix2 ${updated_datafile} \
-      --gtf_file ${gtf_file} \
+      --metadata_file ${metadata_file} \
       --output_prefix ${output_name}/${output_name} 
     tar -zcvf ${output_name}.tar.gz ${output_name}
   }
@@ -33,14 +33,14 @@ workflow RunDataMatrixComparison {
   String base_datafile
   String updated_datafile
   String output_name
-  String gtf_file
+  String metadata_file
 
   call CompareDataMatrix {
     input:
       base_datafile = base_datafile,
       updated_datafile = updated_datafile,
       output_name = output_name,
-      gtf_file = gtf_file,
+      metadata_file = metadata_file,
       src_dir = scripts_dir,
   }
 }
