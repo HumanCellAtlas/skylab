452870e6c3901228b9b3e40557d2a34ffb220d9b
Ambrose J Carr <mail@ambrosejcarr.com>
2018-05-07 21:36:14 -0400

    trigger

---
+++ test/trigger_scientific_test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# note, this test must be run from the workflow root for the relative paths to work out.
+
+set -e
+
+export CROMWELL_SECRETS=~/.ssh/mint_cromwell_config.json
+
+TEST_DIR=./test/optimus/scientific
+
+cromwell-tools run \
+  --wdl-file "${TEST_DIR}"/TestOptimusScientific.wdl \
+  --dependencies-json "${TEST_DIR}"/dependencies.json \
+  --inputs-json "${TEST_DIR}"/test_inputs.json
