514fe73c53213003e3314e3144551e179156061d
Saman Ehsan <sehsan@broadinstitute.org>
2018-12-03 14:55:46 -0500

    Fix slash

--- test/config/render-ctmpls.sh
+++ test/config/render-ctmpls.sh
@@ -17,7 +17,6 @@ docker run --rm -v ${working_dir}:/working \
     -e VAULT_TOKEN=${vault_token} \
     -e INPUT_PATH=/working/test/config \
     -e OUT_PATH=/working/test \
-    --privileged \ 
-    "${docker_image}" bash -c "ls /working && ls /working/test && ls /working/test/config"
+    --privileged "${docker_image}" bash -c "ls /working && ls /working/test && ls /working/test/config"
     
     
