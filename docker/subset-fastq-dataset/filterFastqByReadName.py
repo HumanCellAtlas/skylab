e6341119f36b1b224c309bf782c1bd00a0d31110
Nick Barkas <nikolas.barkas@outlook.com>
2019-03-28 16:37:39 +0000

    fix to script

--- docker/subset-fastq-dataset/filterFastqByReadName.py
+++ docker/subset-fastq-dataset/filterFastqByReadName.py
@@ -3,22 +3,22 @@
 import click
 import sys
 from Bio import SeqIO
-
+import gzip
 
 @click.command()
-@click.option('--in-fastq', help='gz compressed input fastq file')
-@click.option('--out-fastq', help='gz compressed output fastq file')
+@click.option('--in-fastq-gz', help='gz compressed input fastq file')
+@click.option('--out-fastq-gz', help='gz compressed output fastq file')
 @click.option('--keep-reads', help='gz compressed file with read names to keep')
 @click.option('--verbose', help='verbose', default=False, is_flag=True, flag_value=True)
-def filter_by_read_name(in_fastq, out_fastq, keep_reads, verbose):
+def filter_by_read_name(in_fastq_gz, out_fastq_gz, keep_reads, verbose):
     # Put reads that are in the keep list in the output file
-    keep_read_set = set(line.decode('ISO-8859-1').rstrip() for line in open(keep_reads, 'rb'))
+    keep_read_set = set(line.decode('ascii').rstrip() for line in open(keep_reads, 'rb'))
     if verbose:
         print('Done loading keep read list', file=sys.stderr)
 
     # Loop over input and filter reads
-    with open(out_fastq, 'wt') as output_file:
-        with open(in_fastq, 'rt') as input_file:
+    with gzip.open(out_fastq_gz, 'wt') as output_file:
+        with gzip.open(in_fastq_gz, 'rt') as input_file:
             counter = 0
             for rec in SeqIO.parse(input_file, 'fastq'):
                 counter += 1
