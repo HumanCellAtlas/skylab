53d6de5e0af0e22d70728669eb4c0937b3d1d3f3
tlangs <tlangs@broadinstitute.org>
2019-05-07 16:21:58 -0400

    jq foo

--- test/pr_testing/validate_hashes.sh
+++ test/pr_testing/validate_hashes.sh
@@ -8,11 +8,12 @@ declare -r HASH="0cc175b9c0f1b6a831c399e269772661"
 for KEY in $(echo "${KEYS}" | jq -r '.[]')
 do
   echo "KEY: $KEY"
-  if [[ $HASH = $(jq "[$KEY]" $HASHES_FILE) ]]
+  # need `jq -r` to get results without surrounding double-quotes
+  if [[ $HASH = $(jq -r '.["'$KEY'"]' $HASHES_FILE) ]]
   then
     echo "yay"
-#  else
-#    echo "HASH: $HASH"
-#    echo "HASHES_FILE_VALUE: $(jq "$KEY" $HASHES_FILE)"
+  else
+    echo "HASH: $HASH"
+    echo "HASHES_FILE_VALUE: $(jq -r '.["'$KEY'"]' $HASHES_FILE)"
   fi
 done
