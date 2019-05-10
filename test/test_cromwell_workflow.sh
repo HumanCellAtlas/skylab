f2b8748826ab788236a624c1dcfdfc1f30d6cde1
jessicaway <jway@broadinstitute.org>
2019-05-10 11:13:54 -0400

    Add optimus test to circle ci

--- test/test_cromwell_workflow.sh
+++ test/test_cromwell_workflow.sh
@@ -2,26 +2,27 @@
 
 set -e
 
-CROMWELL_USER=$1
-CROMWELL_PASSWORD=$2
-CROMWELL_URL=$3
-INPUTS_JSON=$4
-WDL_FILE=$5
-OPTIONS_FILE=$6
-DEPENDENCIES_JSON=$7
+CROMWELL_KEY_FILE=$1
+CROMWELL_URL=$2
+INPUTS_JSON=$3
+WDL_FILE=$4
+OPTIONS_FILE=$5
+DEPENDENCIES_JSON=$6
+COLLECTION=$7
 # Read list of dependency files into an array to pass to cromwell-tools
 DEPENDENCIES_LIST=$(cat ${DEPENDENCIES_JSON} | python3 -c "import json,sys;obj=json.load(sys.stdin);print(' '.join(obj.values()));")
 echo ${DEPENDENCIES_LIST[@]}
 
 echo "Starting workflow."
+
 WORKFLOW_HASH=$(cromwell-tools submit \
-  --username "${CROMWELL_USER}" \
-  --password "${CROMWELL_PASSWORD}" \
+  --service-account-key "${CROMWELL_KEY_FILE}" \
   --url "${CROMWELL_URL}" \
   --inputs_files "${INPUTS_JSON}" \
   --wdl-file "${WDL_FILE}" \
   --options-file "${OPTIONS_FILE}" \
   --deps-file ${DEPENDENCIES_LIST[@]} \
+  --collectiuon-name ${COLLECTION} \
 )
 
 # Get workflow id from cromwell-tools response: {"id": "XXXXX", "status": "Submitted"}
@@ -35,16 +36,14 @@ sleep 10
 
 echo "Waiting for workflow ${WORKFLOW_ID} to complete..."
 cromwell-tools wait "${WORKFLOW_ID}" \
-  --username "${CROMWELL_USER}" \
-  --password "${CROMWELL_PASSWORD}" \
+  --service-account-key "${CROMWELL_KEY_FILE}" \
   --url "${CROMWELL_URL}" \
   --timeout-minutes 60 \
   --poll-interval-seconds 30
 
 echo "Checking workflow completion status."
 STATUS_RESULT=$(cromwell-tools status \
-  --username "${CROMWELL_USER}" \
-  --password "${CROMWELL_PASSWORD}" \
+  --service-account-key "${CROMWELL_KEY_FILE}" \
   --url "${CROMWELL_URL}" \
   --uuid "${WORKFLOW_ID}" \
 )
