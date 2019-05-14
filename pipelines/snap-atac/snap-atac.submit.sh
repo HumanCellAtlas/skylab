936a3c8c3fa435e8a4b95b8fc5e5c3025747ef4f
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-05-14 15:39:56 -0400

    Handle human and mouse

--- pipelines/snap-atac/snap-atac.submit.sh
+++ pipelines/snap-atac/snap-atac.submit.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+usage()
+{
+cat << EOF
+USAGE: `basename $0` [options]
+    -i  inputs.json (e.g. snap-atac.inputs.mouse.json)
+EOF
+}
+
+while getopts "i:h" OPTION
+do
+    case $OPTION in
+        i) inputs_json=$OPTARG ;;
+        h) usage; exit 1 ;;
+        *) usage; exit 1 ;;
+    esac
+done
+
+if [ -z "$inputs_json" ] || [ ! -r "$inputs_json" ]
+then
+    usage
+    exit 1
+fi
+
 cromwell-tools submit \
     --url "https://cromwell.caas-prod.broadinstitute.org" \
     --collection-name "pipeline-surge" \
     --service-account-key "${HOME}/pipelines_surge_credentials.json" \
     --wdl snap-atac-primary.wdl \
-    --inputs-files snap-atac.inputs.mouse.json \
+    --inputs-files ${inputs_json} \
     --options-file snap-atac.options.json
--- pipelines/snap-atac/snap-atac.validate.sh
+++ pipelines/snap-atac/snap-atac.validate.sh
@@ -3,3 +3,7 @@
 java -jar ${HOME}/Applications/womtool-40.jar \
     validate snap-atac-primary.wdl \
     --inputs snap-atac.inputs.mouse.json
+
+java -jar ${HOME}/Applications/womtool-40.jar \
+    validate snap-atac-primary.wdl \
+    --inputs snap-atac.inputs.human.json
