0fe75d91850513c9502da2e1003eaf8ba9daf217
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-05-14 16:51:22 -0400

    Update

--- pipelines/snap-atac/snap-atac.validate.sh
+++ pipelines/snap-atac/snap-atac.validate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo "mouse"
 java -jar ${HOME}/Applications/womtool-40.jar \
     validate snap-atac-primary.wdl \
     --inputs snap-atac.inputs.mouse.json
 
+echo "human"
 java -jar ${HOME}/Applications/womtool-40.jar \
     validate snap-atac-primary.wdl \
     --inputs snap-atac.inputs.human.json
