b0e4995004ec862a960d9749f6e92cfb3a60fe89
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-05-15 09:38:15 -0400

    Parameterize service account key (-k)

--- pipelines/snap-atac/snap-atac.submit.sh
+++ pipelines/snap-atac/snap-atac.submit.sh
@@ -5,13 +5,15 @@ usage()
 cat << EOF
 USAGE: `basename $0` [options]
     -i  inputs.json (e.g. snap-atac.inputs.mouse.json)
+    -k  service account key (e.g. pipelines_surge_credentials.json)
 EOF
 }
 
-while getopts "i:h" OPTION
+while getopts "i:k:h" OPTION
 do
     case $OPTION in
         i) inputs_json=$OPTARG ;;
+        k) service_account_key=$OPTARG ;;
         h) usage; exit 1 ;;
         *) usage; exit 1 ;;
     esac
@@ -23,10 +25,16 @@ then
     exit 1
 fi
 
+if [ -z "$service_account_key" ] || [ ! -r "$service_account_key" ]
+then
+    usage
+    exit 1
+fi
+
 cromwell-tools submit \
     --url "https://cromwell.caas-prod.broadinstitute.org" \
     --collection-name "pipeline-surge" \
-    --service-account-key "${HOME}/pipelines_surge_credentials.json" \
+    --service-account-key "${service_account_key}" \
     --wdl snap-atac-primary.wdl \
     --inputs-files ${inputs_json} \
     --options-file snap-atac.options.json
