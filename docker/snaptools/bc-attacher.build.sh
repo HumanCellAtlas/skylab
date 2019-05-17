6553fd02461fe263c00416b33f1a982ce09e3d36
Nick Barkas <nikolas.barkas@outlook.com>
2019-05-17 12:22:28 -0400

    remove bc attacher

--- docker/snaptools/bc-attacher.build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build . -f ./bc-attacher.Dockerfile -t bc-attacher
-# docker build . -f ./bc-attacher.Dockerfile -t quay.io/humancellatlas/bc-attacher:0.0.1
-# docker push quay.io/humancellatlas/bc-attacher:0.0.1
