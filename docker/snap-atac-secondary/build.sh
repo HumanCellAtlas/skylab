34e19fded65c61f21b2ea015a469f495b5173001
Nick Barkas <nikolas.barkas@outlook.com>
2019-05-17 12:42:07 -0400

    remove secondary

--- docker/snap-atac-secondary/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# pre-download the necessary dataset for secondary analysis
-# then build them into the docker image
-./get-data.sh
-
-# build and push
-docker build . -t quay.io/humancellatlas/snap-atac-secondary:0.0.1
-docker push quay.io/humancellatlas/snap-atac-secondary:0.0.1
