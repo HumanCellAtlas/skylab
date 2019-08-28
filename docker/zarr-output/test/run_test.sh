b303692823f4cf11b78c1890e264f93825d4377e
Nikolas Barkas <nikolas.barkas@outlook.com>
2019-08-28 14:26:57 -0400

    Update Optimus Zarr Output with EmptyDrops(GL-576) (#248)
    Update Optimus ZARR output to include emptydrops data. In the process, added versioning to the ZARR file to ease parsing and flatened the structure. A new image for this ZARR conversion was added as well as tests. The relevant documentation under docs was updated.

---
+++ docker/zarr-output/test/run_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker build ../ -t tmp-test-image
+docker run -it --mount type=bind,source="${PWD}/data/",target=/data\
+       --mount type=bind,source="${PWD}/testout",target=/testout\
+       --rm tmp-test-image:latest python3 create_zarr_optimus.py\
+            --empty_drops_file /data/empty_drops_result.csv\
+            --cell_metrics /data/merged-cell-metrics.csv.gz\
+            --gene_metrics /data/merged-gene-metrics.csv.gz\
+            --cell_id /data/sparse_counts_row_index.npy\
+            --gene_id /data/sparse_counts_col_index.npy\
+            --count_matrix /data/sparse_counts.npz\
+            --annotation_file /data/gencode.v27.primary_assembly.annotation.gtf.gz \
+            --output_path_for_zarr /testout/output\
+            --sample_id "testsample"\
+            --format DirectoryStore
+#            --verbose
