798d86fa7eccd163f6e45af57e78d8cadffa9b6f
Kishori M Konwar <kkonwar@broadinstitute.org>
2019-09-26 12:02:49 -0400

    removed a local file

--- library/accessory_workflows/build_indices/commands.sh
+++
@@ -1,2 +0,0 @@
-#MOUSE
-cromwell-tools submit --url "https://cromwell.caas-prod.broadinstitute.org" --collection-name "pipeline-surge" --service-account-key /Users/kkonwar/.ssh/pipelines_surge_credentials.json --wdl BuildIndices.wdl -i mouse_inputs.json -l mouse-build-label.json 
