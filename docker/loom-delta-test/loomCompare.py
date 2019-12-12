0dd609c89339b7c1f8db08e3895430b375a9b2cf
Nick Barkas <nikolas.barkas@outlook.com>
2019-12-12 15:54:31 -0500

    address PR review comments

--- docker/loom-delta-test/loomCompare.py
+++ docker/loom-delta-test/loomCompare.py
@@ -9,8 +9,8 @@ import numpy as np
 def main():
     description = """This script compares two loom files and checks that they contain identical data up to a constant"""
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("--truth-loom", dest="truth_loom_path", required=True, help="Path to truth loom file")
-    parser.add_argument("--check-loom", dest="check_loom_path", required=True, help="Path to loom file to check")
+    parser.add_argument("--truth-loom", dest="truth_loom_path", required=True, help="Path to truth loom file", type=str)
+    parser.add_argument("--check-loom", dest="check_loom_path", required=True, help="Path to loom file to check", type=str)
     parser.add_argument("--delta-cutoff", dest="delta_cutoff", required=True, help="Max delta value allowed", type=int)
     args = parser.parse_args()
     truth_loom = loompy.connect(args.truth_loom_path)
@@ -22,10 +22,10 @@ def main():
     delta = np.sum(np.absolute(np.subtract(truth_loom_array, check_loom_array)))
 
     if delta < args.delta_cutoff:
-        print("Matrices are identical")
+        print(f"Matrices are identical: delta: {delta} delta_cutoff: {args.delta_cutoff}")
         sys.exit(0)
     else:
-        print("Matrices are not identical")
+        print(f"Matrices are NOT identical: delta: {delta} delta_cutoff: {args.delta_cutoff}")
         sys.exit(1)
 
 
