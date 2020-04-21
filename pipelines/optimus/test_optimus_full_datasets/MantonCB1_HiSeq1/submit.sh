1060f613d23e42662ef347e79da60ff6ae6a032d
ekiernan <55763654+ekiernan@users.noreply.github.com>
2020-04-21 08:30:06 -0400

    Add files via upload

---
+++ pipelines/optimus/test_optimus_full_datasets/MantonCB1_HiSeq1/submit.sh
@@ -0,0 +1,12 @@
+skylab_path="/Users/nbarkas/work/optimus/runs_Mar19/skylab"
+
+cromwell-tools submit \
+	       --username `vault_mint_cromwell_login` \
+	       --password `vault_mint_cromwell_password` \
+	       -w $skylab_path/pipelines/optimus/Optimus.wdl \
+	       -d $skylab_path/library/tasks/* \
+	       -l labels.json \
+	       -o options.json \
+	       --url https://cromwell.mint-dev.broadinstitute.org/ \
+	       -i inputs_CB1_HiSeq_1.json |
+               tee log.txt
